4d83e34f393c4204b709fb14185852be6931c253
Noufel <nboucheneb77@gmail.com>
2025-03-02 05:02:59 +0100

    Jpp

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,15 +1,25 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { BASE_ONION_ROUTER_PORT } from "../config";
+import { BASE_ONION_ROUTER_PORT, REGISTRY_PORT } from "../config";
+import axios from "axios";
+import { generateRsaKeyPair, exportPubKey, exportPrvKey } from "../crypto";
 
+let lastReceivedEncryptedMessage: string | null = null;
+let lastReceivedDecryptedMessage: string | null = null;
+let lastMessageDestination: number | null = null;
+let lastReceivedMessage: string | null = null;
+let lastSentMessage: string | null = null;
 
 export async function simpleOnionRouter(nodeId: number) {
+  const { publicKey, privateKey } = await generateRsaKeyPair();
+  const pubKeyString = await exportPubKey(publicKey);
+  const prvKeyString = await exportPrvKey(privateKey);
 
-  let lastReceivedEncryptedMessage: string | null = null;
-  let lastReceivedDecryptedMessage: string | null = null;
-  let lastMessageDestination: number | null = null;
-  let lastReceivedMessage: string | null = null;
-  let lastSentMessage: string | null = null;
+  // Register the node on the registry
+  await axios.post(`http://localhost:${REGISTRY_PORT}/registerNode`, {
+    nodeId,
+    pubKey: pubKeyString,
+  });
 
   const onionRouter = express();
   onionRouter.use(express.json());
@@ -39,7 +49,9 @@ export async function simpleOnionRouter(nodeId: number) {
     res.status(200).json({ result: lastSentMessage });
   });
 
-  
+  onionRouter.get("/getPrivateKey", (req, res) => {
+    res.status(200).json({ result: prvKeyString });
+  });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
